9e5287c37ddbd1515858ce3628e03708649b9024
aChiweiHuang <chiwei.huang@access-company.com>
2019-12-30 18:37:19 +0800

    add CatShopSupervisor in OtpApplication children list

--- elixir_workshop/otp/lib/otp_application.ex
+++ elixir_workshop/otp/lib/otp_application.ex
@@ -3,7 +3,8 @@ defmodule OtpApplication do
 
   def start(_type, _args) do
     children = [
-      {StackSupervisor, strategy: :one_for_one, name: StackSupervisor}
+      {StackSupervisor, strategy: :one_for_one, name: StackSupervisor},
+      {CatShopSupervisor, strategy: :one_for_one, name: CatShopSupervisor}
     ]
 
     Supervisor.start_link(children, strategy: :rest_for_one, name: __MODULE__)
